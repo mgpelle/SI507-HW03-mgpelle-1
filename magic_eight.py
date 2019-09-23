31d5c6b5ccee2fa95fbdfb9b9cc6fe36c475fa81
ham6erger <55514943+ham6erger@users.noreply.github.com>
2019-09-23 16:59:01 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,13 @@
 response = input("What is your question?:")
-def ask_question(response):
+def ask_question():
     return response
 
+
 import random
-answer=["It is certain","It is decidedly so.","Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good","Very doubtful."]
-print(random.choice(answer))
+def answer_reponse(response):
+    answer=["It is certain","It is decidedly so.","Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good","Very doubtful."]
+    result=random.choice(answer)
+    print(result)
+    return result
+
+answer_reponse(response)
